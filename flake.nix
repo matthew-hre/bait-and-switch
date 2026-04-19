7d80e67805452124b960b584a9f902a0c8c127da
Matthew Hrehirchuk <me@matthew-hre.com>
2026-04-19 09:46:02 -0600

    ci: setup builds

--- flake.nix
+++ flake.nix
@@ -11,6 +11,21 @@
   }: let
     eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
   in {
+    packages = eachSystem (pkgs: {
+      default = pkgs.stdenv.mkDerivation {
+        pname = "bait-and-switch";
+        version = "0.1";
+        src = ./.;
+        nativeBuildInputs = [pkgs.makeWrapper];
+        installPhase = ''
+          mkdir -p $out/share/bait-and-switch $out/bin
+          cp -r main.lua conf.lua src assets $out/share/bait-and-switch/
+          makeWrapper ${pkgs.love}/bin/love $out/bin/bait-and-switch \
+            --add-flags "$out/share/bait-and-switch"
+        '';
+      };
+    });
+
     devShells = eachSystem (pkgs: {
       default = pkgs.mkShell {
         buildInputs = with pkgs; [
